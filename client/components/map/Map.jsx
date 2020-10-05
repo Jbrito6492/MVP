00161e8cbb1064fa6cf6f4d9d922f896ded171d9
Josh Brito <jbrito6492@gmail.com>
2020-10-05 00:05:48 -0700

    editing map component

--- client/components/map/Map.jsx
+++ client/components/map/Map.jsx
@@ -4,9 +4,9 @@ import {
   GoogleMap,
   useLoadScript,
   Marker,
-  InfoWindow,
+  Circle,
 } from "@react-google-maps/api";
-
+import { FaMapMarkerAlt } from "react-icons/fa";
 const libraries = ["places"];
 const mapContainerStyle = {
   width: "100%",
@@ -21,8 +21,8 @@ const options = {
 
 const Map = (props) => {
   const [center, setCenter] = useState({ lat: 0, lng: 0 });
-  const [markers, setMarkers] = useState([]);
-  const [path, setPath] = useState([{}]);
+  const [marker, setMarker] = useState(null);
+  const [selected, setSelected] = useState(null);
 
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
@@ -31,7 +31,7 @@ const Map = (props) => {
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(success);
-  }, []);
+  }, [setMarker]);
 
   const success = (pos) => {
     const { latitude: lat, longitude: lng } = pos.coords;
@@ -41,8 +41,21 @@ const Map = (props) => {
     });
   };
 
+  const onMapClick = useCallback((e) => {
+    setMarker({
+      lat: e.latLng.lat(),
+      lng: e.latLng.lng(),
+    });
+  }, []);
+
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
   if (loadError) return "error loading maps";
   if (!isLoaded) return "loading maps";
+
   return (
     <div>
       <GoogleMap
@@ -50,16 +63,24 @@ const Map = (props) => {
         zoom={13}
         center={center}
         options={options}
-        onClick={(e) => {
-          setMarkers((current) => [
-            ...current,
-            { lat: e.latLng.lat(), lng: e.latLng.lng() },
-          ]);
-        }}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
       >
-        {markers.map((marker, index) => (
-          <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
-        ))}
+        {marker ? (
+          <div>
+            <Marker
+              position={{ lat: marker.lat, lng: marker.lng }}
+              onClick={() => {
+                setMarker(null);
+              }}
+            />
+            <Circle
+              options={{ fillColor: "red" }}
+              center={{ lat: marker.lat, lng: marker.lng }}
+              radius={1200}
+            />
+          </div>
+        ) : null}
       </GoogleMap>
     </div>
   );
