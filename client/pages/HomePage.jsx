5a9167edbcdde50ad5583034cb761d7dcbf6311b
Josh Brito <jbrito6492@gmail.com>
2020-10-07 14:02:54 -0700

    refactored HomePage to functional component

--- client/pages/HomePage.jsx
+++ client/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { startSession } from "../store/actions/index.js";
 import Room from "../components/room/Room.jsx";
@@ -6,67 +6,20 @@ import UserList from "../components/user_list/UserList.jsx";
 import Header from "../components/header/Header.jsx";
 import { Redirect } from "react-router-dom";
 
-@connect((store) => {
-  return {
-    auth: store.auth,
+const HomePage = (props) => {
+  const renderView = () => {
+    return (
+      <div>
+        <div>
+          <Header />
+        </div>
+        <div className="container">
+          <Room />
+        </div>
+      </div>
+    );
   };
-})
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.renderView = this.renderView.bind(this);
-  }
+  return <div>{renderView()}</div>;
+};
 
-  componentDidMount() {
-    this.props.dispatch(startSession());
-  }
-
-  renderView() {
-    const { auth } = this.props;
-    if (auth === null) {
-      return (
-        <div>
-          <div className="preloader-wrapper big active">
-            <div className="spinner-layer spinner-blue">
-              <div className="circle-clipper left">
-                <div className="circle"></div>
-              </div>
-              <div className="gap-patch">
-                <div className="circle"></div>
-              </div>
-              <div className="circle-clipper right">
-                <div className="circle"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-    } else if (auth === false) {
-      return (
-        <div>
-          <div>
-            <Header />
-          </div>
-          <div className="container">
-            <Redirect to="/login" />
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <div>
-            <Header />
-          </div>
-          <div className="container">
-            <Room />
-          </div>
-        </div>
-      );
-    }
-  }
-
-  render() {
-    return <div>{this.renderView()}</div>;
-  }
-}
+export default HomePage;
