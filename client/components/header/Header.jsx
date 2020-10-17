56087da0af4410034d11727c198938eedb7e1bf1
Josh Brito <jbrito6492@gmail.com>
2020-10-16 17:01:13 -0700

    adjusted input in Header search bar

--- client/components/header/Header.jsx
+++ client/components/header/Header.jsx
@@ -22,10 +22,11 @@ import h from "../../css/header.css";
 const Header = ({ setShowNavigation, showNavigation }) => {
   const dispatch = useDispatch();
   const { isAuthenticated, username } = useSelector((state) => state.auth);
-  const [search, setSearch] = useState({});
+  const [search, setSearch] = useState({ hashtag: "#" });
 
   const handleChange = (e) => {
     setSearch({ ...search, [e.target.name]: e.target.value });
+    console.log(search)
   };
 
   const handleSubmit = (e) => {
--- database/controllers/hashtag.js
+++ database/controllers/hashtag.js
@@ -2,7 +2,6 @@ const Hashtag = require('../models/hashtag.js');
 
 exports.createHashtag = (req, res) => {
   const { hashtag } = req.body;
-  // check to see if hashtag exists
   let options = { upsert: true, new: true, setDefaultsOnInsert: true };
   Hashtag.findOneAndUpdate({ hashtag }, { $inc: { activeUsers: 1 } }, options).exec()
     .then(result => {
