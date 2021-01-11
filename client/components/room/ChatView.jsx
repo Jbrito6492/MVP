67e38298e56611d4ab386a01c1c13cd89daeee7a
Josh Brito <jbrito6492@gmail.com>
2021-01-11 16:16:36 -0700

    messages now show who they are from

--- client/components/room/ChatView.jsx
+++ client/components/room/ChatView.jsx
@@ -6,31 +6,30 @@ import profilePic from "../../images/example-profile.png";
 const ChatView = ({ isTyping, chat, username }) => {
   return (
     <div className={styles.chatMessageList}>
-      <div className={`${styles.youMessage} ${styles.messageRow}`}>
-        <div className={styles.messageContent}>
-          {chat.map((message, index) => {
-            if (username === message.username) {
+      <div className={styles.messageContent}>
+        {chat.map((message, index) => {
+          return username === message.username ? (
+            <div className={`${styles.youMessage} ${styles.messageRow}`}>
               <div key={index} className={styles.youMessageContainer}>
-              <div className={styles.messageText}>{message.message}</div>
-              <div className={styles.messageTime}>{message.date}</div>
-            </div>;
-            } else {
-
-            }
-          })}
-          {isTyping && (
-            <div className={styles.youMessageContainer}>
-              <div className={`${styles.loading} ${styles.messageText}`}></div>
+                <div className={styles.messageText}>{message.message}</div>
+                <div className={styles.messageTime}>{message.date}</div>
+              </div>
             </div>
-          )}
-        </div>
-      </div>
-      <div className={`${styles.messageRow} ${styles.otherMessage}`}>
-        <div className={styles.messageContent}>
-          <img src={profilePic} />
-          <div className={styles.messageText}>whats happening</div>
-          <div className={styles.messageTime}>Oct 10</div>
-        </div>
+          ) : (
+            <div className={`${styles.messageRow} ${styles.otherMessage}`}>
+              <div className={styles.messageContent}>
+                <img src={profilePic} />
+                <div className={styles.messageText}>{message.message}</div>
+                <div className={styles.messageTime}>{message.date}</div>
+              </div>
+            </div>
+          );
+        })}
+        {isTyping && (
+          <div className={styles.youMessageContainer}>
+            <div className={`${styles.loading} ${styles.messageText}`}></div>
+          </div>
+        )}
       </div>
     </div>
   );
